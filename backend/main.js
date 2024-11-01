4bf9bd9bed698a9e345867fe27138102fcd442d2
unknown <you@example.com>
2024-11-01 17:12:08 +0700

    first init

---
+++ backend/main.js
@@ -0,0 +1,28 @@
+/*npm i nodemon json-server@0.17.0*/
+import jsonServer from "json-server";
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// Add custom routes before JSON Server router
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
+
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+// Use default router
+server.use(router);
+server.listen(5000, () => {
+  console.log("JSON Server is running");
+});
